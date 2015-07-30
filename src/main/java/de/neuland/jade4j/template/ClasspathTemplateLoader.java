c216fbd5200fcd466cb02b54cc112adc4fa4dd5c
Danny Purcell <danny.purcell@cerner.com>
2015-07-29 22:11:32 -0500

    Adds reloading and Charset support to ClasspathTemplateLoader

--- src/main/java/de/neuland/jade4j/template/ClasspathTemplateLoader.java
+++ src/main/java/de/neuland/jade4j/template/ClasspathTemplateLoader.java
@@ -3,35 +3,63 @@ package de.neuland.jade4j.template;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.util.MissingResourceException;
 
 /**
  * Loads a Jade template from Classpath
  * It is useful when Jade templates are in the same JAR or WAR
- * 
- * @author emiguel
  *
+ * Supports reloading on modification when the requested template is not in a JAR or WAR.
+ *
+ * @author emiguel
  */
 public class ClasspathTemplateLoader implements TemplateLoader {
 
-    private static final String suffix = ".jade";
-    private String encoding = "UTF-8";
+    private static final String SUFFIX = ".jade";
+    private Charset charset;
 
-    public long getLastModified(String name) {
-        return -1;
+    public ClasspathTemplateLoader() {
+        this(Charset.defaultCharset());
+    }
+
+    public ClasspathTemplateLoader(Charset encoding) {
+        setEncoding(encoding);
+    }
+
+    @Override
+    public long getLastModified(String name) throws IOException {
+        return locateResource(name).openConnection().getLastModified();
     }
 
     @Override
     public Reader getReader(String name) throws IOException {
-        if(!name.endsWith(suffix))name = name + suffix;
-        return new InputStreamReader(Thread.currentThread().getContextClassLoader().getResourceAsStream(name), getEncoding());
+        assert name != null && !name.isEmpty() : "name may not be null or empty";
+        return new InputStreamReader(locateResource(name).openStream(), charset);
     }
 
     public String getEncoding() {
-        return encoding;
+        return charset.name();
     }
 
     public void setEncoding(String encoding) {
-        this.encoding = encoding;
+        assert encoding != null && !encoding.isEmpty() : "charset may not be null or empty";
+        setEncoding(Charset.forName(encoding));
     }
 
+    public void setEncoding(Charset charset) {
+        this.charset = charset;
+    }
+
+    private URL locateResource(String name) {
+        if (!name.toLowerCase().endsWith(SUFFIX)) {
+            name = name + SUFFIX;
+        }
+        final URL url = Thread.currentThread().getContextClassLoader().getResource(name);
+        if (url == null) {
+            throw new MissingResourceException("could not locate resource on classpath: " + name, name, name);
+        }
+        return url;
+    }
 }
